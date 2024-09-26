5b690224275e02412bcf5c42b493b06c5d16fe3a
Tristan Kennedy <tristan@taskinoz.com>
2024-09-26 19:10:53 +1000

    Move to astro

--- db.ts
+++
@@ -1,7 +0,0 @@
-// db.js
-import sqlite3 from 'sqlite3';
-export const db = new sqlite3.Database(':memory:');
-
-db.serialize(() => {
-  db.run("CREATE TABLE parking_spots (id INTEGER PRIMARY KEY, date TEXT, spot INTEGER, rego TEXT, make TEXT, user_name TEXT)");
-});
